4b1a45a5572a202f8940853a4570481732ef3b26
NguyenNhatHao2004 <hao.nn.64cntt@ntu.edu.vn>
2025-02-23 09:49:21 +0700

    Ex3_SimpleSumApp
    (2) Xử lý backend: Bắt/Đáp ứng sự kiện bằng hàm xử lý cài đặt cho OnClick trên XML

--- Ex3_SimpleSumApp/app/src/main/java/hao/edu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/hao/edu/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,8 @@
 package hao.edu.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        //gan layout tuong ung
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XuLyCong(View view)//lang nghe va xu li sk click len nut tinh tong
+    {
+        //tim , tham chieu den dieu khien xml
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        //lay du lieu ve
+        String strA = editTextSoA.getText().toString(); //"2"
+
+        String strB = editTextSoB.getText().toString(); //"4"
+
+        //chuyen du lieu sanng dang so
+        int so_A = Integer.parseInt(strA); //2
+        int so_B = Integer.parseInt(strB); //4
+        //tinh toan theo yeu cau
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        //hien ra man hinh
+        editTextKQ.setText(strTong);
     }
 }
