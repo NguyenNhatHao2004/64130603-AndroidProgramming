b7fd50899a544d219620731ffc63c1831a6dc25a
NguyenNhatHao2004 <hao.nn.64cntt@ntu.edu.vn>
2025-02-23 19:42:32 +0700

    Ex4_AddSubMulDiv_Onclick
    (2) Định nghĩa hàm xử lý ờ tệp Java và Khai báo hàm đó ở tệp Layout XML qua thuộc tính android:onClick="Hàm xử lý"

--- Ex4_AddSubMulDiv_Onclick/app/src/main/java/hao/edu/ex4_addsubmuldiv_onclick/MainActivity.java
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/hao/edu/ex4_addsubmuldiv_onclick/MainActivity.java
@@ -1,6 +1,9 @@
 package hao.edu.ex4_addsubmuldiv_onclick;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //khai bao doi tuong gan voi dieu khien tuong ung o day
+    EditText edittextso1;
+    EditText edittextso2;
+    EditText edittextkq;
+
+    Button NutCong,NutTru,NutNhan,NutChia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,110 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
+    }
+
+    void TimDieuKhien()
+    {
+        EditText edittextso1 =(EditText) findViewById(R.id.edtso1);
+        EditText edittextso2 =(EditText) findViewById(R.id.edtso2);
+        EditText edittextkq =(EditText) findViewById(R.id.edtkq);
+        NutCong = (Button) findViewById(R.id.btncong);
+        NutTru = (Button) findViewById(R.id.btntru);
+        NutNhan = (Button) findViewById(R.id.btnnhan);
+        NutChia = (Button) findViewById(R.id.btnchia);
+    }
+    public void XuLyCong(View v)
+    {
+        //code xu ly cong
+        //b1 lay du lieu hia so
+        //b1.1 tim edittext so 1 va
+
+        //b1.2 lAy du lieu tu 2 dkhien do
+        String Sothu1 = edittextso1.getText().toString();
+        String Sothu2 = edittextso2.getText().toString();
+        //b1.3 chuyen du lieu thanh so
+        float soA = Float.parseFloat(Sothu1);
+        float soB = Float.parseFloat(Sothu2);
+        //tinh toan
+        float tong = soA +soB;
+        //b3 hien ket qua
+        //b3.1 tim edit text kq
+
+        //b3.2 chuan bi cdu lieu de xuat, bien thanh dang chuoi
+        String ChuoiKQ = String.valueOf(tong);
+        //b3.3 gan ket qua len dieu khien
+        edittextkq.setText(ChuoiKQ);
+    }
+   public void XuLyTru(View v)
+    {
+        //code xu ly tru
+        //code xu ly cong
+        //b1 lay du lieu hia so
+        //b1.1 tim edittext so 1 va 2
+
+        //b1.2 lAy du lieu tu 2 dkhien do
+        String Sothu1 = edittextso1.getText().toString();
+        String Sothu2 = edittextso2.getText().toString();
+        //b1.3 chuyen du lieu thanh so
+        float soA = Float.parseFloat(Sothu1);
+        float soB = Float.parseFloat(Sothu2);
+        //tinh toan
+        float tru = soA - soB;
+        //b3 hien ket qua
+        //b3.1 tim edit text kq
+
+        //b3.2 chuan bi cdu lieu de xuat, bien thanh dang chuoi
+        String ChuoiKQ = String.valueOf(tru);
+        //b3.3 gan ket qua len dieu khien
+        edittextkq.setText(ChuoiKQ);
+    }
+   public void XuLyNhan(View v)
+    {
+        //code xu ly nhan
+        //code xu ly cong
+        //b1 lay du lieu hia so
+        //b1.1 tim edittext so 1 va 2
+
+        //b1.2 lAy du lieu tu 2 dkhien do
+        String Sothu1 = edittextso1.getText().toString();
+        String Sothu2 = edittextso2.getText().toString();
+        //b1.3 chuyen du lieu thanh so
+        float soA = Float.parseFloat(Sothu1);
+        float soB = Float.parseFloat(Sothu2);
+        //tinh toan
+        float nhan = soA * soB;
+        //b3 hien ket qua
+        //b3.1 tim edit text kq
+
+        //b3.2 chuan bi cdu lieu de xuat, bien thanh dang chuoi
+        String ChuoiKQ = String.valueOf(nhan);
+        //b3.3 gan ket qua len dieu khien
+        edittextkq.setText(ChuoiKQ);
+
+    }
+   public void XuLyChia(View v)
+    {
+        //code xu ly chia
+        //code xu ly cong
+        //b1 lay du lieu hia so
+        //b1.1 tim edittext so 1 va 2
+
+        //b1.2 lAy du lieu tu 2 dkhien do
+        String Sothu1 = edittextso1.getText().toString();
+        String Sothu2 = edittextso2.getText().toString();
+        //b1.3 chuyen du lieu thanh so
+        float soA = Float.parseFloat(Sothu1);
+        float soB = Float.parseFloat(Sothu2);
+        //tinh toan
+        float chia = soA / soB;
+        //b3 hien ket qua
+        //b3.1 tim edit text kq
+
+        //b3.2 chuan bi cdu lieu de xuat, bien thanh dang chuoi
+        String ChuoiKQ = String.valueOf(chia);
+        //b3.3 gan ket qua len dieu khien
+        edittextkq.setText(ChuoiKQ);
+
     }
 }
