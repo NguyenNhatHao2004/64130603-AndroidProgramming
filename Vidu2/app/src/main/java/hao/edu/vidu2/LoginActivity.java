3feb7845757bf37c49df5619316d1f33d7041ea2
NguyenNhatHao2004 <hao.nn.64cntt@ntu.edu.vn>
2025-03-10 20:33:10 +0700

    Login update

--- Vidu2/app/src/main/java/hao/edu/vidu2/LoginActivity.java
+++ Vidu2/app/src/main/java/hao/edu/vidu2/LoginActivity.java
@@ -1,9 +1,11 @@
 package hao.edu.vidu2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,7 +36,18 @@ public class LoginActivity extends AppCompatActivity {
                 //b1.tim tham chieu den dk
                 EditText edtTenDN = (EditText) findViewById(R.id.edtUserName);
                 EditText edtPass = (EditText) findViewById(R.id.edtPass);
-                
+                String tenDangNhap = edtTenDN.getText().toString();
+                String mk = edtPass.getText().toString();
+                //ktra mat khau
+                if (tenDangNhap.equals("nguyennhathao")&& mk.equals("123"))
+                {
+                    Intent iQuiz = new Intent(LoginActivity.this,HomeActivity.class);
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap",mk);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Wrong",Toast.LENGTH_LONG);
+                }
             }
         });
     }
