fa12275271ab4b78a171482beca77c7b94ac5823
NguyenNhatHao2004 <hao.nn.64cntt@ntu.edu.vn>
2025-04-04 15:07:05 +0700

    chỉnh sửa nôi dung lần cuối

--- ThiGK2Nguyennhathao/app/src/main/java/thigk2/nguyennhathao/ActivityChucNang4.java
+++ ThiGK2Nguyennhathao/app/src/main/java/thigk2/nguyennhathao/ActivityChucNang4.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ActivityChucNang4 extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> RecyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,21 @@ public class ActivityChucNang4 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        RecyclerViewDatas = getDataForRecylerView();
+
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        landScapeAdapter = new LandScapeAdapter(this, RecyclerViewDatas);
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+    ArrayList<LandScape> getDataForRecylerView()
+    {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        dsDuLieu.add(new LandScape("z5293328518459_003_44dba3aa3d71a90524837e18fe3e9b80.jpg","Pháo bông"));
+        dsDuLieu.add(new LandScape("ca_phe_sua.jpg","cà phê sữa"));
+        dsDuLieu.add(new LandScape("capuchino.jpg","capuchino"));
+        return dsDuLieu;
     }
 }
