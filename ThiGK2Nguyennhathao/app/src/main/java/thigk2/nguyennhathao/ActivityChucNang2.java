d5f5042d5d9176fb00d0ba4fe862335348afc1d7
NguyenNhatHao2004 <hao.nn.64cntt@ntu.edu.vn>
2025-04-04 14:18:24 +0700

    Xong màn 2

--- ThiGK2Nguyennhathao/app/src/main/java/thigk2/nguyennhathao/ActivityChucNang2.java
+++ ThiGK2Nguyennhathao/app/src/main/java/thigk2/nguyennhathao/ActivityChucNang2.java
@@ -1,6 +1,8 @@
 package thigk2.nguyennhathao;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,26 @@ public class ActivityChucNang2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void XuLyCong(View view)//lang nghe va xu li sk click len nut tinh tong
+    {
+        //tim , tham chieu den dieu khien xml
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        //lay du lieu ve
+        String strA = editTextSoA.getText().toString(); //"2"
+
+        String strB = editTextSoB.getText().toString(); //"4"
+
+        //chuyen du lieu sanng dang so
+        int so_A = Integer.parseInt(strA); //2
+        int so_B = Integer.parseInt(strB); //4
+        //tinh toan theo yeu cau
+        int tong = (so_A+so_B)/2 ;
+        String strTong = String.valueOf(tong);
+        //hien ra man hinh
+        editTextKQ.setText(strTong);
     }
 }
